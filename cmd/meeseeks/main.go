c3e2bc62c6002514922b888aa76572bc38b63f61
Gustavo Caso <gustavocaso@gmail.com>
2025-09-24 17:08:10 +0200

    set version via goreleaser

--- cmd/meeseeks/main.go
+++ cmd/meeseeks/main.go
@@ -24,6 +24,8 @@ import (
 	"github.com/GustavoCaso/meeseeks/internal/logger"
 )
 
+var Version = "development"
+
 func main() {
 	if len(os.Args) < 2 {
 		printUsage()
@@ -54,7 +56,7 @@ func main() {
 	case "reload":
 		cmdErr = reloadCommand(args, logger)
 	case "version":
-		fmt.Fprintln(os.Stdout, "meeseeks version 1.0.0")
+		fmt.Fprintf(os.Stdout, "meeseeks version %s\n", Version)
 	case "-h", "--help":
 		printUsage()
 	default:
